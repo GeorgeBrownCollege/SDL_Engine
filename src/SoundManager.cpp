b9f9f278e1708bf4715d0fba7f859cf0378368d7
TheProfezzzor <TheProfezzzor@users.noreply.github.com>
2020-05-16 13:20:37 -0400

    v1 of updated SoundManager
    Added multiple methods with default parameters. Most notable change from before is an explicit quit() method that cleans up.

--- src/SoundManager.cpp
+++ src/SoundManager.cpp
@@ -1,15 +1,17 @@
 #include "SoundManager.h"
-
-SoundManager* SoundManager::s_pInstance;
+#include <algorithm>
 
 SoundManager::SoundManager()
 {
-	Mix_OpenAudio(22050, AUDIO_S16, 2, (4096 / 2));
+	Mix_OpenAudio(22050, AUDIO_S16SYS, 2, 2048);
+	panReset();
 }
 
-SoundManager::~SoundManager()
+SoundManager::~SoundManager(){}
+
+void SoundManager::allocateChannels(const int channels) const
 {
-	Mix_CloseAudio();
+	Mix_AllocateChannels(channels);
 }
 
 bool SoundManager::load(std::string fileName, std::string id, sound_type type)
@@ -17,36 +19,135 @@ bool SoundManager::load(std::string fileName, std::string id, sound_type type)
 	if (type == SOUND_MUSIC)
 	{
 		Mix_Music* pMusic = Mix_LoadMUS(fileName.c_str());
-		if (pMusic == 0)
+		if (pMusic == nullptr)
 		{
 			std::cout << "Could not load music: ERROR - " << Mix_GetError() << std::endl;
 			return false;
 		}
-
 		m_music[id] = pMusic;
 		return true;
 	}
 	else if (type == SOUND_SFX)
 	{
 		Mix_Chunk* pChunk = Mix_LoadWAV(fileName.c_str());
-		if (pChunk == 0)
+		if (pChunk == nullptr)
 		{
 			std::cout << "Could not load SFX: ERROR - " << Mix_GetError() << std::endl;
 			return false;
 		}
-
 		m_sfxs[id] = pChunk;
 		return true;
 	}
 	return false;
 }
 
-void SoundManager::playMusic(std::string id, int loop)
+void SoundManager::unload(std::string id, sound_type type)
 {
-	Mix_PlayMusic(m_music[id], loop);
+	if (type == SOUND_MUSIC && m_music.find(id) != m_music.end())
+	{
+		Mix_FreeMusic(m_music[id]);
+		m_music.erase(id);
+	}
+	else if (type == SOUND_SFX && m_sfxs.find(id) != m_sfxs.end())
+	{
+		Mix_FreeChunk(m_sfxs[id]);
+		m_sfxs.erase(id);
+	}
+	else
+		std::cout << "Could not unload '" << id << "' - id not found!" << std::endl;
 }
 
-void SoundManager::playSound(std::string id, int loop)
+void SoundManager::playMusic(std::string id, int loop/* = -1 */, int fadeIn/* = 0 */)
+{	
+	std::cout << "Playing music..." << fadeIn << std::endl;
+	if (Mix_FadeInMusic(m_music[id], loop, fadeIn) == -1)
+		std::cout << "Unable to play music: ERROR - " << Mix_GetError() << std::endl;
+}
+
+void SoundManager::stopMusic(int fadeOut/* = 0 */) const
 {
-	Mix_PlayChannel(-1, m_sfxs[id], loop);
+	if (Mix_PlayingMusic())
+		Mix_FadeOutMusic(fadeOut);
+}
+
+void SoundManager::pauseMusic() const
+{
+	if (Mix_PlayingMusic())
+		Mix_PauseMusic();
+}
+
+void SoundManager::resumeMusic() const
+{
+	if (Mix_PausedMusic())
+		Mix_ResumeMusic();
+}
+
+void SoundManager::playSound(std::string id, int loop/* = 0 */, int channel/* = -1 */)
+{
+	if (Mix_PlayChannel(channel, m_sfxs[id], loop) == -1)
+		std::cout << "Unable to play SFX: ERROR - " << Mix_GetError() << std::endl;
+}
+
+void SoundManager::setMusicVolume(const int vol) const
+{
+	if (vol >= 0 && vol <= 128)
+		Mix_VolumeMusic(vol);
+}
+
+void SoundManager::setSoundVolume(const int vol) const
+{
+	if (vol >= 0 && vol <= 128)
+		Mix_Volume(-1, vol);
+}
+
+void SoundManager::setAllVolume(const int vol) const
+{
+	setMusicVolume(vol);
+	setSoundVolume(vol);
+}
+
+void SoundManager::panLeft(const unsigned increment, const int channel)
+{
+	panSet(m_pan - increment, channel);
+}
+
+void SoundManager::panRight(const unsigned increment, const int channel)
+{
+	panSet(m_pan + increment, channel);
+}
+
+void SoundManager::panReset(const int channel)
+{
+	panSet(75, channel);
+}
+
+void SoundManager::panSet(const int amount, const int channel)
+{
+	std::cout << "Pan:" << amount << std::endl;
+	m_pan = std::max(0,std::min(amount,100)); // Old clamp.
+	std::cout << "Pan:" << m_pan << std::endl;
+	int leftVol = (255 * std::min((m_pan - 100.0) / (50.0 - 100.0), 1.0)); // z-min/max-min
+	std::cout << "Left:" << leftVol << std::endl;
+	int rightVol = (255 * std::min(m_pan / 50.0, 1.0));
+	std::cout << "Right:" << rightVol << std::endl;
+	Mix_SetPanning(channel, leftVol, rightVol);
+}
+
+void SoundManager::quit()
+{
+	// Clean up sound effects.
+	if (Mix_Playing(-1))
+		Mix_HaltChannel(-1); // Halt all channels.
+	for (auto const& i : m_sfxs)
+		Mix_FreeChunk(m_sfxs[i.first]);
+	m_sfxs.clear();
+	// Clean up music.
+	if (Mix_PlayingMusic())
+		Mix_HaltMusic();
+	for (auto const& i : m_music)
+		Mix_FreeMusic(m_music[i.first]);
+	m_music.clear();
+	// Quit.
+	Mix_CloseAudio();
+	Mix_Quit();
 }
--- src/SoundManager.h
+++ src/SoundManager.h
@@ -17,30 +17,37 @@ enum sound_type
 
 class SoundManager {
 public:
-	static SoundManager* Instance()
+	static SoundManager& Instance()
 	{
-		if (s_pInstance == 0)
-		{
-			s_pInstance = new SoundManager();
-		}
-		return s_pInstance;
+		static SoundManager instance; // Magic statics.
+		return instance;
 	}
-
+	void allocateChannels(const int channels) const;
 	bool load(std::string fileName, std::string id, sound_type type);
-	void playSound(std::string id, int loop);
-	void playMusic(std::string id, int loop);
-private:
+	void unload(std::string id, sound_type type);
+	void playMusic(std::string id, int loop = -1, int fadeIn = 0);
+	void stopMusic(int fadeOut = 0) const;
+	void pauseMusic() const;
+	void resumeMusic() const;
+	void playSound(std::string id, int loop = 0, int channel = -1);
+	void setMusicVolume(const int vol) const;
+	void setSoundVolume(const int vol) const;
+	void setAllVolume(const int vol) const;
+	// Panning converts a single slider value from 0 to 100 to the stereo speaker volumes.
+	void panLeft(const unsigned increment = 5, const int channel = MIX_CHANNEL_POST);
+	void panRight(const unsigned increment = 5, const int channel = MIX_CHANNEL_POST);
+	void panReset(const int channel = MIX_CHANNEL_POST);
+	void panSet(const int amount, const int channel = MIX_CHANNEL_POST);
+	void quit();
+private: // Methods.
+	SoundManager();
+	SoundManager(const SoundManager&);
+	~SoundManager();
+private: // Properties.
 	static SoundManager* s_pInstance;
-
 	std::map<std::string, Mix_Chunk*> m_sfxs;
 	std::map<std::string, Mix_Music*> m_music;
-
-	SoundManager();
-	~SoundManager();
-
-	SoundManager(const SoundManager&);
+	int m_pan; // A slider value from 0 to 100. 0 = full left, 100 = full right.
 };
-
-typedef SoundManager TheSoundManager;
 
 #endif /* defined (__SoundManager__) */
