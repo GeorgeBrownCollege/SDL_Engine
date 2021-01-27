bc1261fef6b5ebea9aeff0d6f2aedd9175526c55
personisgeneric <72466024+personisgeneric@users.noreply.github.com>
2021-01-26 19:01:27 -0500

    Add files via upload
    Dog idle animation and walking animation implemented. Idle cycle not quite right since he sits back up, will look into make new enum states. Working on bg soon.

--- src/Player.cpp
+++ src/Player.cpp
@@ -4,13 +4,13 @@
 Player::Player() : m_currentAnimationState(PLAYER_IDLE_RIGHT) {
 	TextureManager::Instance()->loadSpriteSheet(
 		"../Assets/sprites/atlas.txt",
-		"../Assets/sprites/atlas.png",
-		"spritesheet");
+		"../Assets/sprites/dog-spritesheet.png",
+		"dogsprite");
 
-	setSpriteSheet(TextureManager::Instance()->getSpriteSheet("spritesheet"));
+	setSpriteSheet(TextureManager::Instance()->getSpriteSheet("dogsprite"));
 
 	// set frame width
-	SetWidth(53);
+	SetWidth(87);
 
 	// set frame height
 	SetHeight(58);
@@ -35,20 +35,20 @@ void Player::Draw() {
 	// draw the player according to animation state
 	switch (m_currentAnimationState) {
 		case PLAYER_IDLE_RIGHT:
-			TextureManager::Instance()->playAnimation("spritesheet", GetAnimation("idle"),
-													  x, y, 0.12f, 0, 255, true);
-			break;
-		case PLAYER_IDLE_LEFT:
-			TextureManager::Instance()->playAnimation("spritesheet", GetAnimation("idle"),
+			TextureManager::Instance()->playAnimation("dogsprite", GetAnimation("idle"),
 													  x, y, 0.12f, 0, 255, true, SDL_FLIP_HORIZONTAL);
 			break;
+		case PLAYER_IDLE_LEFT:
+			TextureManager::Instance()->playAnimation("dogsprite", GetAnimation("idle"),
+													  x, y, 0.12f, 0, 255, true);
+			break;
 		case PLAYER_RUN_RIGHT:
-			TextureManager::Instance()->playAnimation("spritesheet", GetAnimation("run"),
-													  x, y, 0.25f, 0, 255, true);
+			TextureManager::Instance()->playAnimation("dogsprite", GetAnimation("run"),
+													  x, y, 0.25f, 0, 255, true, SDL_FLIP_HORIZONTAL);
 			break;
 		case PLAYER_RUN_LEFT:
-			TextureManager::Instance()->playAnimation("spritesheet", GetAnimation("run"),
-													  x, y, 0.25f, 0, 255, true, SDL_FLIP_HORIZONTAL);
+			TextureManager::Instance()->playAnimation("dogsprite", GetAnimation("run"),
+													  x, y, 0.25f, 0, 255, true);
 			break;
 		default:
 			break;
@@ -68,20 +68,23 @@ void Player::m_buildAnimations() {
 	Animation idleAnimation = Animation();
 
 	idleAnimation.m_name = "idle";
-	idleAnimation.m_frames.push_back(GetSpriteSheet()->GetFrame("megaman-idle-0"));
-	idleAnimation.m_frames.push_back(GetSpriteSheet()->GetFrame("megaman-idle-1"));
-	idleAnimation.m_frames.push_back(GetSpriteSheet()->GetFrame("megaman-idle-2"));
-	idleAnimation.m_frames.push_back(GetSpriteSheet()->GetFrame("megaman-idle-3"));
-
+	idleAnimation.m_frames.push_back(GetSpriteSheet()->GetFrame("dog-idle-0"));
+	idleAnimation.m_frames.push_back(GetSpriteSheet()->GetFrame("dog-idle-1"));
+	idleAnimation.m_frames.push_back(GetSpriteSheet()->GetFrame("dog-idle-2"));
+	idleAnimation.m_frames.push_back(GetSpriteSheet()->GetFrame("dog-idle-3"));
+	idleAnimation.m_frames.push_back(GetSpriteSheet()->GetFrame("dog-idle-4"));
+	idleAnimation.m_frames.push_back(GetSpriteSheet()->GetFrame("dog-idle-5"));
+	idleAnimation.m_frames.push_back(GetSpriteSheet()->GetFrame("dog-idle-6"));
 	setAnimation(idleAnimation);
 
 	Animation runAnimation = Animation();
 
 	runAnimation.m_name = "run";
-	runAnimation.m_frames.push_back(GetSpriteSheet()->GetFrame("megaman-run-0"));
-	runAnimation.m_frames.push_back(GetSpriteSheet()->GetFrame("megaman-run-1"));
-	runAnimation.m_frames.push_back(GetSpriteSheet()->GetFrame("megaman-run-2"));
-	runAnimation.m_frames.push_back(GetSpriteSheet()->GetFrame("megaman-run-3"));
+	runAnimation.m_frames.push_back(GetSpriteSheet()->GetFrame("dog-dash-0"));
+	runAnimation.m_frames.push_back(GetSpriteSheet()->GetFrame("dog-dash-1"));
+	runAnimation.m_frames.push_back(GetSpriteSheet()->GetFrame("dog-dash-2"));
+	runAnimation.m_frames.push_back(GetSpriteSheet()->GetFrame("dog-dash-3"));
+	runAnimation.m_frames.push_back(GetSpriteSheet()->GetFrame("dog-dash-4"));
 
 	setAnimation(runAnimation);
 }
