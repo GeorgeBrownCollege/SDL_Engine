351bff63daef60cff6adda36a458b66186e95d9b
Mustafa <Qureshi>
2021-01-30 22:54:18 -0500

    Added movement with acceleration and deceleration
    Two functions were added to Player.h: Move and Decelerate

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,4 +1,6 @@
 #include "Player.h"
+#include <iostream>
+#include "EventManager.h"
 #include "TextureManager.h"
 
 Player::Player() : m_currentAnimationState(PLAYER_IDLE_RIGHT) {
@@ -19,6 +21,10 @@ Player::Player() : m_currentAnimationState(PLAYER_IDLE_RIGHT) {
 	GetRigidBody()->velocity = glm::vec2(0.0f, 0.0f);
 	GetRigidBody()->acceleration = glm::vec2(0.0f, 0.0f);
 	GetRigidBody()->isColliding = false;
+
+	SetAccelerationRate(1.0f);
+	SetMaxSpeed(8.25f);
+	
 	SetType(PLAYER);
 
 	m_buildAnimations();
@@ -56,13 +62,30 @@ void Player::Draw() {
 
 }
 
-void Player::Update() { }
+void Player::Update() {
+
+	if (EventManager::Instance().isKeyDown(SDL_SCANCODE_A))
+	{
+		Move(false);
+	}
+	else if (EventManager::Instance().isKeyDown(SDL_SCANCODE_D))
+	{
+		Move(true);
+	}
+	else
+	{
+		Decellerate();
+	}
+	
+}
 
 void Player::Clean() { }
 
+// Setters
 void Player::setAnimationState(const PlayerAnimationState new_state) {
 	m_currentAnimationState = new_state;
 }
+
 
 void Player::m_buildAnimations() {
 	Animation idleAnimation = Animation();
@@ -85,3 +108,48 @@ void Player::m_buildAnimations() {
 
 	setAnimation(runAnimation);
 }
+
+void Player::Move(bool _direction) {
+
+	GetRigidBody()->acceleration.x = m_accelerationRate;
+
+	if (GetRigidBody()->velocity.x != 0)
+		GetRigidBody()->velocity.x = abs(GetRigidBody()->velocity.x) + 0.5f * GetRigidBody()->acceleration.x;
+	else
+		GetRigidBody()->velocity.x = 0.5f * GetRigidBody()->acceleration.x;
+
+	// If the player wants to move left the velocity will be turned into a negative
+	if (_direction == false)
+		GetRigidBody()->velocity.x *= -1;
+	
+	// if the absolute value of the new velocity is greater than the max speed the velocity will be set to the max speed in the proper direction
+	abs(GetRigidBody()->velocity.x) < m_maxSpeed ? GetRigidBody()->velocity.x = GetRigidBody()->velocity.x :
+		_direction == false ? GetRigidBody()->velocity.x = -m_maxSpeed : GetRigidBody()->velocity.x = m_maxSpeed;
+	
+	GetTransform()->position += GetRigidBody()->velocity;
+}
+
+void Player::Decellerate() {
+
+	float decellerateRate = 0.2f;
+	
+	if (GetRigidBody()->velocity.x != 0)
+	{
+		if (GetRigidBody()->velocity.x < 1.0f && GetRigidBody()->velocity.x > -1.0f)
+			GetRigidBody()->velocity.x = 0.0f;
+
+		// If the player's velocity is not equal to zero, it's velocity will be decreased until it's zero
+		GetRigidBody()->velocity.x == 0 ? GetRigidBody()->velocity.x == GetRigidBody()->velocity.x :
+			GetRigidBody()->velocity.x < 0 ? GetRigidBody()->velocity.x += abs(GetRigidBody()->velocity.x * decellerateRate) : GetRigidBody()->velocity.x -= abs(GetRigidBody()->velocity.x * decellerateRate);
+
+		GetTransform()->position += GetRigidBody()->velocity;
+	}
+}
+
+// Setters
+void Player::SetAccelerationRate(float _accel) { m_accelerationRate = _accel; }
+void Player::SetMaxSpeed(float _speed) { m_maxSpeed = _speed; }
+
+// Getters
+float Player::GetAcceleration() { return m_accelerationRate; }
+float Player::GetMaxSpeed() { return m_maxSpeed; }
