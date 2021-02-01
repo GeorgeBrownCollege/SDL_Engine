b7ac9fa0ef7db1f4164a4a72a02d7dd07a84e9c8
Nick Oostdyk <NicholasOostdyk@gmail.com>
2021-02-01 17:17:51 -0500

    Formatting

--- src/PlayScene.cpp
+++ src/PlayScene.cpp
@@ -7,18 +7,17 @@
 #include "imgui_sdl.h"
 #include "Renderer.h"
 
-PlayScene::PlayScene()
-{
+PlayScene::PlayScene() {
 	PlayScene::Start();
 }
 
 PlayScene::~PlayScene()
 = default;
 
-void PlayScene::Draw()
-{
-	if(EventManager::Instance().isIMGUIActive())
-	{
+void PlayScene::Draw() {
+
+	if (EventManager::Instance().isIMGUIActive()) {
+
 		GUI_Function();
 	}
 
@@ -26,44 +25,31 @@ void PlayScene::Draw()
 	SDL_SetRenderDrawColor(Renderer::Instance()->getRenderer(), 255, 255, 255, 255);
 }
 
-void PlayScene::Update()
-{
+void PlayScene::Update() {
 	UpdateDisplayList();
 }
 
-void PlayScene::Clean()
-{
+void PlayScene::Clean() {
 	RemoveAllChildren();
 }
 
-void PlayScene::HandleEvents()
-{
+void PlayScene::HandleEvents() {
 	EventManager::Instance().update();
 
 	// handle player movement with GameController
-	if (SDL_NumJoysticks() > 0)
-	{
-		if (EventManager::Instance().getGameController(0) != nullptr)
-		{
+	if (SDL_NumJoysticks() > 0) {
+		if (EventManager::Instance().getGameController(0) != nullptr) {
 			const auto deadZone = 10000;
-			if (EventManager::Instance().getGameController(0)->LEFT_STICK_X > deadZone)
-			{
+			if (EventManager::Instance().getGameController(0)->LEFT_STICK_X > deadZone) {
 				m_pPlayer->setAnimationState(PLAYER_RUN_RIGHT);
 				m_playerFacingRight = true;
-			}
-			else if (EventManager::Instance().getGameController(0)->LEFT_STICK_X < -deadZone)
-			{
+			} else if (EventManager::Instance().getGameController(0)->LEFT_STICK_X < -deadZone) {
 				m_pPlayer->setAnimationState(PLAYER_RUN_LEFT);
 				m_playerFacingRight = false;
-			}
-			else
-			{
-				if (m_playerFacingRight)
-				{
+			} else {
+				if (m_playerFacingRight) {
 					m_pPlayer->setAnimationState(PLAYER_IDLE_RIGHT);
-				}
-				else
-				{
+				} else {
 					m_pPlayer->setAnimationState(PLAYER_IDLE_LEFT);
 				}
 			}
@@ -72,53 +58,40 @@ void PlayScene::HandleEvents()
 
 
 	// handle player movement if no Game Controllers found
-	if (SDL_NumJoysticks() < 1)
-	{
-		if (EventManager::Instance().isKeyDown(SDL_SCANCODE_A))
-		{
+	if (SDL_NumJoysticks() < 1) {
+		if (EventManager::Instance().isKeyDown(SDL_SCANCODE_A)) {
 			m_pPlayer->setAnimationState(PLAYER_RUN_LEFT);
 			m_playerFacingRight = false;
-		}
-		else if (EventManager::Instance().isKeyDown(SDL_SCANCODE_D))
-		{
+		} else if (EventManager::Instance().isKeyDown(SDL_SCANCODE_D)) {
 			m_pPlayer->setAnimationState(PLAYER_RUN_RIGHT);
 			m_playerFacingRight = true;
-		}
-		else
-		{
-			if (m_playerFacingRight)
-			{
+		} else {
+			if (m_playerFacingRight) {
 				m_pPlayer->setAnimationState(PLAYER_IDLE_RIGHT);
-			}
-			else
-			{
+			} else {
 				m_pPlayer->setAnimationState(PLAYER_IDLE_LEFT);
 			}
 		}
 	}
-	
 
-	if (EventManager::Instance().isKeyDown(SDL_SCANCODE_ESCAPE))
-	{
+
+	if (EventManager::Instance().isKeyDown(SDL_SCANCODE_ESCAPE)) {
 		TheGame::Instance()->quit();
 	}
 
-	if (EventManager::Instance().isKeyDown(SDL_SCANCODE_1))
-	{
+	if (EventManager::Instance().isKeyDown(SDL_SCANCODE_1)) {
 		TheGame::Instance()->changeSceneState(START_SCENE);
 	}
 
-	if (EventManager::Instance().isKeyDown(SDL_SCANCODE_2))
-	{
+	if (EventManager::Instance().isKeyDown(SDL_SCANCODE_2)) {
 		TheGame::Instance()->changeSceneState(END_SCENE);
 	}
 }
 
-void PlayScene::Start()
-{
+void PlayScene::Start() {
 	// Set GUI Title
 	m_guiTitle = "Play Scene";
-	
+
 	// Plane Sprite
 	m_pPlaneSprite = new Plane();
 	AddChild(m_pPlaneSprite);
@@ -131,19 +104,16 @@ void PlayScene::Start()
 	// Back Button
 	m_pBackButton = new Button("../Assets/textures/backButton.png", "backButton", BACK_BUTTON);
 	m_pBackButton->GetTransform()->position = glm::vec2(300.0f, 400.0f);
-	m_pBackButton->AddEventListener(CLICK, [&]()-> void
-	{
+	m_pBackButton->AddEventListener(CLICK, [&]()-> void {
 		m_pBackButton->setActive(false);
 		TheGame::Instance()->changeSceneState(START_SCENE);
 	});
 
-	m_pBackButton->AddEventListener(MOUSE_OVER, [&]()->void
-	{
+	m_pBackButton->AddEventListener(MOUSE_OVER, [&]()->void {
 		m_pBackButton->setAlpha(128);
 	});
 
-	m_pBackButton->AddEventListener(MOUSE_OUT, [&]()->void
-	{
+	m_pBackButton->AddEventListener(MOUSE_OUT, [&]()->void {
 		m_pBackButton->setAlpha(255);
 	});
 	AddChild(m_pBackButton);
@@ -151,19 +121,16 @@ void PlayScene::Start()
 	// Next Button
 	m_pNextButton = new Button("../Assets/textures/nextButton.png", "nextButton", NEXT_BUTTON);
 	m_pNextButton->GetTransform()->position = glm::vec2(500.0f, 400.0f);
-	m_pNextButton->AddEventListener(CLICK, [&]()-> void
-	{
+	m_pNextButton->AddEventListener(CLICK, [&]()-> void {
 		m_pNextButton->setActive(false);
 		TheGame::Instance()->changeSceneState(END_SCENE);
 	});
 
-	m_pNextButton->AddEventListener(MOUSE_OVER, [&]()->void
-	{
+	m_pNextButton->AddEventListener(MOUSE_OVER, [&]()->void {
 		m_pNextButton->setAlpha(128);
 	});
 
-	m_pNextButton->AddEventListener(MOUSE_OUT, [&]()->void
-	{
+	m_pNextButton->AddEventListener(MOUSE_OUT, [&]()->void {
 		m_pNextButton->setAlpha(255);
 	});
 
